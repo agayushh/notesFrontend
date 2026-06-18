2a5ae93124356d2ff42d09b0165e75fe82a131b7
agayushh <agayush088@gmail.com>
2026-06-18 19:11:25 +0530

    feat: integrate features grid section into landing page

--- app/pages/home.tsx
+++ app/pages/home.tsx
@@ -1,19 +1,27 @@
+import { Features } from "../pageComponent/features";
+import { FeaturesGrid } from "../pageComponent/featuresGrid";
 import { Header } from "../pageComponent/header";
-import { Navbar } from "../pageComponent/navbar";
 import { LowerHeader } from "../pageComponent/lowerHeader";
+import { Navbar } from "../pageComponent/navbar";
 
 export const LandingPage = () => {
   return (
-    <div className="flex flex-col w-screen">
-      <div className="flex items-center w-screen justify-center">
+    <div className="flex flex-col w-full">
+      <div className="flex items-center w-full justify-center px-4">
         <Navbar />
       </div>
 
-      <div className="flex items-center justify-center">
+      <div className="flex items-center justify-center w-full">
         <Header />
       </div>
-      <div className="flex items-center justify-center">
+      <div className="w-full">
         <LowerHeader />
+      </div>
+      <div className="w-full">
+        <Features />
+      </div>
+      <div className="w-full">
+        <FeaturesGrid />
       </div>
     </div>
   );
